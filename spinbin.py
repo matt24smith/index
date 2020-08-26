bd30bf5a3b72629ae22e22855b0d9c5106679b72
Matt Smith <matthew.smith@dal.ca>
2020-08-26 19:32:53 -0300

    Update spinbin.py

--- spinbin.py
+++ spinbin.py
@@ -2,6 +2,7 @@ import os
 import json
 import pickle
 import sqlite3
+import logging
 from hashlib import md5
 from multiprocessing import Pool
 from datetime import datetime, timedelta
@@ -12,8 +13,8 @@ from numpy import arange
 
 class Spinbin():
     """ job scheduler utility featuring:
-            - spatial indexing, binning, and hashing for spatial processing
-            - in-memory storage of arbitrary binary objects
+            - spatial hashing, indexing, and binning for spatial processing
+            - in-memory caching for arbitrary binary objects
             - parallel process management (see inherited: ParallelSpinbin)
     
         intended to be a lightweight utility for hashmap-like boolean 
@@ -28,30 +29,49 @@ class Spinbin():
     # compute 8-bit integer hash for a given dictionary
     hash_dict = lambda self, kwargs, seed='': int(md5((str(seed) + json.dumps(kwargs, sort_keys=True, default=str)).encode('utf-8')).hexdigest(), base=16) >> 80
 
-    def __init__(self, /, *, dx=2, dy=2, dz=5000, dt=timedelta(days=1), storagedir=os.getcwd(), inmemory=False, bins=True, **kwargs): 
+    def __init__(self, /, *, pool=1, store=False, inmemory=False, bins=True, dx=2, dy=2, dz=5000, dt=timedelta(days=1), storagedir=os.getcwd(), **kwargs): 
         """
             args:
+                pool:
+                    number of processes to run in parallel. when not using the
+                    ParallelSpinbin subclass, this must be equal to 1.
+                store:
+                    determines whether to store the results of the callback.
+                    when False, no binary will be serialized. only a hash of the 
+                    function and input arguments will be stored to determine
+                    whether the callback was called
+                inmemory:
+                    when store=True and inmemory=True, callback results can be 
+                    stored to an in-memory database. this allows for faster
+                    read and update times for processing that requires frequent
+                    updates. will be copied to disk upon __exit__
+                bins:
+                    if True, boundary kwargs supplied to __init__ will be 
+                    split into multiple smaller area bins and passed to the 
+                    callback function. when 
                 dx:
-                    delta longitude bin size (int)
+                    delta longitude bin size (float)
                 dy: 
-                    delta latitude bin size (int)
+                    delta latitude bin size (float)
                 dz:
-                    delta depth bin size (int)
+                    delta depth bin size (float)
                 dt:
                     delta time bin size (timedelta)
                 storagedir:
-                    filepath location to store checksums (string)
+                    directory to store spatial hashes and binary objects
                **kwargs:
-                    boundary arguments
+                    boundary arguments to split into bins, e.g. 
+                    (south, west, north, east, top, bottom, start, end)
         """
-        assert os.path.isdir(str(storagedir)), f'invalid dir {storagedir}'
         assert kwargs != {} or bins == False, 'no boundaries provided'
-
-        self.storagedir, self.inmemory = storagedir, inmemory
+        self.store, self.pool, self.storagedir, self.inmemory, = store, pool, storagedir, inmemory
         self.storage = os.path.join(storagedir, 'checksums.db') if not inmemory else ':memory:'
         self.kwargslist = list(self.bin_kwargs(dx, dy, dz, dt, **kwargs)) if bins else [kwargs]
+        #breakpoint()
 
     def __enter__(self):
+        assert self.kwargslist != [], 'empty kwargs!'
+        assert os.path.isdir(str(self.storagedir)), f'invalid dir {storagedir}'
         with sqlite3.connect(self.storage) as con:
             db = con.cursor()
             db.execute('CREATE TABLE IF NOT EXISTS hashmap(hash INT NOT NULL, bytes BLOB)')
@@ -67,6 +87,7 @@ class Spinbin():
 
     def __call_generator__(self, /, *, callback, **passkwargs):
         seed=f'{callback.__module__}.{callback.__name__}:{json.dumps(passkwargs, default=str, sort_keys=True)}'
+        assert self.pool == 1, 'use ParallelSpinbin for processing pool'
         for kwargs in self.kwargslist: 
             if not self.serialized(kwargs, seed): self.insert_hash(kwargs, seed, callback(**passkwargs, **kwargs))
             elif self.inmemory: self.update_hash(kwargs, seed, callback(**passkwargs, **kwargs))
@@ -79,11 +100,13 @@ class Spinbin():
         assert not self.inmemory, 'feature not yet implemented'  
 
     def insert_hash(self, kwargs={}, seed='', obj=None):
+        logging.debug(f'INSERTING SPATIAL HASH {self.hash_dict(kwargs, seed)}\n{seed = }\nBIN: {kwargs = }')
         with sqlite3.connect(self.storage) as con:
             db = con.cursor()
-            db.execute('INSERT INTO hashmap VALUES (?,?)', (self.hash_dict(kwargs, seed), pickle.dumps(obj)))
+            db.execute('INSERT INTO hashmap VALUES (?,?)', (self.hash_dict(kwargs, seed), bytes(pickle.dumps(obj) if self.store else pickle.dumps(None))))
 
     def update_hash(self, kwargs={}, seed='', obj=None):
+        logging.debug(f'UPDATING SPATIAL HASH {self.hash_dict(kwargs, seed)}\n{seed = }\nBIN: {kwargs = }')
         with sqlite3.connect(self.storage) as con:
             db = con.cursor()
             db.execute('UPDATE hashmap SET bytes = ? WHERE hash = ?', (pickle.dumps(obj), self.hash_dict(kwargs, seed)))
@@ -94,6 +117,7 @@ class Spinbin():
             db = con.cursor()
             db.execute('SELECT * FROM hashmap WHERE hash == ?', (self.hash_dict(kwargs, seed),))
             res = db.fetchone()
+        logging.debug(f'CHECKING SPATIAL HASH {self.hash_dict(kwargs, seed)}: {"HIT!" if res is not None else "MISS!" }\n{seed = }\nBIN: {kwargs = }')
         if res is None: return False
         if res[1] is None: return True
         if res[1] is not None: return res[1]
@@ -106,7 +130,7 @@ class Spinbin():
 
             args:
                 deltas: (float or timedelta)
-                    bin size of axes
+                    bin size of axes 
 
                 **kwargs (dict) 
                     boundary arguments, e.g.
@@ -117,15 +141,16 @@ class Spinbin():
             yields: 
                 dictionary containing args as a subset of input boundaries
         """
+        if 'start' not in kwargs.keys(): kwargs['start'] = datetime(2000,1,1) 
+        if 'end' not in kwargs.keys(): kwargs['end'] = kwargs['start']+dt
 
-        spacebins = lambda a, b, delta: arange(min(a,b)-(min(a,b)%(delta*1)), max(a,b)-(max(a,b)%(delta*-1)), delta)
-
-        for axmin, axmax, delta in zip(('west','south','top','start',), ('east','north','bottom','end'), (dx,dy,dz,dt)): 
+        for axmin, axmax, delta in zip(('west','south','bottom','start'), ('east','north','top','end'), (dx,dy,dz,dt)): 
             if axmin not in kwargs.keys(): kwargs[axmin] = 0
             if axmax not in kwargs.keys(): kwargs[axmax] = 0
             if min(kwargs[axmin],kwargs[axmax]) == max(kwargs[axmin],kwargs[axmax]): kwargs[axmax] += delta
 
         # spin to win!
+        spacebins = lambda a,b,delta: arange(min(a,b)-(min(a,b)%(delta*1)), max(a,b)-(max(a,b)%(delta*-1)), delta)
         for x in spacebins(kwargs['west'], kwargs['east'], dx):
             for y in spacebins(kwargs['south'], kwargs['north'], dy): 
                 for z in spacebins(kwargs['top'], kwargs['bottom'], dz):
@@ -136,30 +161,8 @@ class Spinbin():
 class ParallelSpinbin(Spinbin):
     """ run spinbin jobs in a parallel processing pool """
 
-    def __init__(self, /, *, pool=1, dx=2, dy=2, dz=5000, dt=timedelta(days=1), storagedir=os.getcwd(), inmemory=False, bins=True, **kwargs): 
-        """
-            args:
-                dx:
-                    delta longitude bin size (int)
-                dy: 
-                    delta latitude bin size (int)
-                dz:
-                    delta depth bin size (int)
-                dt:
-                    delta time bin size (timedelta)
-                storagedir:
-                    filepath location to store checksums (string)
-               **kwargs:
-                    boundary arguments
-        """
-        assert os.path.isdir(storagedir)
-        assert kwargs != {} or bins == False, 'no boundaries provided'
-
-        self.pool, self.storagedir, self.inmemory, = pool, storagedir, inmemory
-        self.storage = os.path.join(storagedir, 'checksums.db') if not inmemory else ':memory:'
-        self.kwargslist = list(self.bin_kwargs(dx, dy, dz, dt, **kwargs)) if bins else [kwargs]
-
     def __call__(self, /, *, callback, **passkwargs):  # https://docs.python.org/3/whatsnew/3.8.html
+        assert len(self.kwargslist) > 1, 'nothing to parallelize when bins=False'
         with Pool(self.pool) as p: 
             return list(p.map(self.__call_generator__, zip((callback for _ in self.kwargslist), self.kwargslist, (passkwargs for _ in self.kwargslist))))
 
@@ -182,7 +185,6 @@ if __name__ == '__main__':
 
         >   python3.8 spinbin.py 
     """
-
     import time
 
     def callback(**kwargs):
@@ -193,8 +195,9 @@ if __name__ == '__main__':
 
     def parallelized_callback(**kwargs):
         """ another useless demo function """
-        print(f'hello world! i am parallel! {kwargs = }')  # https://docs.python.org/3/whatsnew/3.8.html
+        print(f'hello world! i am parallel! {kwargs = }')
         time.sleep(1)
+        return None
 
     # define some boundaries using or subsetting these dict keys (only used when bins=True)
     kwargs = {
@@ -206,7 +209,7 @@ if __name__ == '__main__':
         }
 
     # here kwargs will split into 21 function calls using default spatial bin sizes
-    with Spinbin(bins=True, inmemory=False, **kwargs) as scheduler: 
+    with Spinbin(bins=True, store=True, inmemory=False, **kwargs) as scheduler: 
         results = scheduler(callback=callback, testarg='some arg', anotherarg='changing this will invalidate results hash')
 
     print(results)
